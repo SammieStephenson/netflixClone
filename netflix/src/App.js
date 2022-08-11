a89083f0f1ababd845b235961ed786965abd6acf
Sammie Stephenson <102987799+SammieStephenson@users.noreply.github.com>
2022-08-11 15:20:27 -0400

    Update App.js

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+import Moviedesc from "./components/moviedesc/Moviedesc";
 import Navbar from "./Navbar"
 // import Home from "./Home"
 import Login from "./pages/login/Login"
@@ -25,34 +25,5 @@ function App() {
   {/* <Footer /> */}
 </>
   ) 
-=======
-// import Navbar from "./Navbar"
-import Moviedesc from "./components/moviedesc/Moviedesc";
-import Login from "./pages/login/Login";
-// import Gallery from "./Gallery"
-import { Route, Routes } from "react-router-dom"
-// import Footer from "./Footer"
-
-
-function App() {
-  return (
-  //  <Login/>
-  <Moviedesc/>
-  )
-    // <>
-      {/* <Navbar /> */}
-      {/* <div className="container"> */}
-
-        
-        {/* <Routes> */}
-          {/* <Route path="/" element={<Home />} /> */}
-          {/* <Route path="/login" element={<Login />} /> */}
-          {/* <Route path="/gallery" element={<Gallery />} /> */}
-        {/* </Routes> */}
-      {/* </div> */}
-      {/* <Footer /> */}
-    // </>
-  
->>>>>>> edc86e553227a7578c14d3e113feb50149518694
 }
 export default App;
