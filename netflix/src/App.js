e6636542452eedea593f134086d310966415b2c9
Natalie  Ruiz <natalie.r0821@Natalies-Air.lan>
2022-08-11 14:22:33 -0400

    landing

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,22 +1,28 @@
 import Navbar from "./Navbar"
-import Home from "./Home"
-import Login from "./Login"
-import Gallery from "./Gallery"
+// import Home from "./Home"
+import Login from "./pages/login/Login"
+// import Gallery from "./Gallery"
 import { Route, Routes } from "react-router-dom"
-import Footer from "./Footer"
+// import Footer from "./Footer"
+import React from 'react';
+import './App.css';
+import Landing from './pages/landing/Landing';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/gallery" element={<Gallery />} />
-        </Routes>
-      </div>
-      <Footer />
-    </>
-  );
+    
+  <>
+  <Navbar /> 
+  <div className="container">
+    <Routes>
+      
+      <Route path="/login" element={<Login />} />
+      <Route exact path="/" element={<Landing />} />
+      {/* <Route path="/gallery" element={<Gallery />} /> */}
+    </Routes>
+  </div>
+  {/* <Footer /> */}
+</>
+  ) 
 }
+export default App;
